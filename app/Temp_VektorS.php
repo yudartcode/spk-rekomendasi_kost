9f8616b495b1a492888e2a413307b0715048a7ed
agungsptr <agung.eka1999@gmail.com>
2019-12-28 20:49:20 +0800

    temp vektor s

---
+++ app/Temp_VektorS.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Temp_VektorS extends Model
+{
+    protected $table = 'temp_vektors';
+    public $timestamps = false;
+}
+}
