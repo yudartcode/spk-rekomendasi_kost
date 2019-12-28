b646b8fb375c2bb9b412c73c90b302e6bc835335
agungsptr <agung.eka1999@gmail.com>
2019-12-28 14:31:37 +0800

    controller

---
+++ app/Http/Controllers/Kost/KostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Kost;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KostController extends Controller
+{
+    public function NormalisasiMatrix()
+    {
+        
+    }
+}
