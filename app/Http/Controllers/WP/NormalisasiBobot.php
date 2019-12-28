8fb824f58af8f264a1ef336ab41ee813a3ebee05
Yuda Karnaen <yuda.karnaen11@gmail.com>
2019-12-28 20:43:37 +0800

    min satu

--- app/Http/Controllers/WP/NormalisasiBobot.php
+++ app/Http/Controllers/WP/NormalisasiBobot.php
@@ -13,9 +13,9 @@ class NormalisasiBobot extends Controller
         $bobot = Temp_Bobot::all();
         $bobotNormal = [];
         foreach ($bobot as $k) {
-            $bobotNormal[0] = $k->jarak_kampus / ($k->jarak_kampus + $k->jarak_market + $k->harga + $k->kebersihan + $k->keamanan + $k->fasilitas);
-            $bobotNormal[1] = $k->jarak_market / ($k->jarak_kampus + $k->jarak_market + $k->harga + $k->kebersihan + $k->keamanan + $k->fasilitas);
-            $bobotNormal[2] = $k->harga / ($k->jarak_kampus + $k->jarak_market + $k->harga + $k->kebersihan + $k->keamanan + $k->fasilitas);
+            $bobotNormal[0] = ($k->jarak_kampus / ($k->jarak_kampus + $k->jarak_market + $k->harga + $k->kebersihan + $k->keamanan + $k->fasilitas))*-1;
+            $bobotNormal[1] = ($k->jarak_market / ($k->jarak_kampus + $k->jarak_market + $k->harga + $k->kebersihan + $k->keamanan + $k->fasilitas))*-1;
+            $bobotNormal[2] = ($k->harga / ($k->jarak_kampus + $k->jarak_market + $k->harga + $k->kebersihan + $k->keamanan + $k->fasilitas))*-1;
             $bobotNormal[3] = $k->kebersihan / ($k->jarak_kampus + $k->jarak_market + $k->harga + $k->kebersihan + $k->keamanan + $k->fasilitas);
             $bobotNormal[4] = $k->keamanan / ($k->jarak_kampus + $k->jarak_market + $k->harga + $k->kebersihan + $k->keamanan + $k->fasilitas);
             $bobotNormal[5] = $k->fasilitas / ($k->jarak_kampus + $k->jarak_market + $k->harga + $k->kebersihan + $k->keamanan + $k->fasilitas);
