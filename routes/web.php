60235a6d029a48d03c26472e0929adfca19b0efc
agungsptr <agung.eka1999@gmail.com>
2020-01-02 23:13:58 +0800

    rapikan route

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/kost', 'KostController@index');
+
+Route::get('/kost', 'KostController@index')->name('kost');
 Route::get('/bobot', 'BobotKontroller@index')->name('bobot');
 Route::post('bobot/store', 'BobotKontroller@store')->name('bobot.store');
+
 //ahp
-Route::get('/normalisasi-kriteria-ahp', 'Ahp\NormalisasiKriteria@do');
-Route::get('/uji-konsistensi-ahp', 'Ahp\UjiKonsistensi@do');
+Route::get('/normalisasi-kriteria-ahp', 'Ahp\NormalisasiKriteria@do')->name('ahp.nor');
+Route::get('/uji-konsistensi-ahp', 'Ahp\UjiKonsistensi@do')->name('ahp.uk');
+
 //topsis
-Route::get('/normalisasi-alternatif-topsis', 'Topsis\NormalisasiMatrix@do');
-Route::get('/solusi-ideal-topsis', 'Topsis\SolusiIdeal@solusiIdeal');
-Route::get('/nilai-preferensi-topsis', 'Topsis\NilaiPreferensi@do');
+Route::get('/normalisasi-alternatif-topsis', 'Topsis\NormalisasiMatrix@do')->name('topsis.nor');
+Route::get('/solusi-ideal-topsis', 'Topsis\SolusiIdeal@solusiIdeal')->name('topsis.si');
+Route::get('/nilai-preferensi-topsis', 'Topsis\NilaiPreferensi@do')->name('topsis.np');
 
